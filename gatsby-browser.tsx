3dad2f3597239a20f0916b7c61fe51143a2ddcca
rewieer <rewieer@gmail.com>
2022-05-26 20:13:34 +0200

    initial commit

---
+++ gatsby-browser.tsx
@@ -0,0 +1,5 @@
+import "@fontsource/raleway/900.css";
+import "@fontsource/source-sans-pro/600.css";
+import "normalize.css";
+
+console.log("ah ok");
--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,28 +3,42 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Sunstack`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.sunstack.io`,
   },
-  plugins: ["gatsby-plugin-styled-components", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": "G-RV7TTTM173"
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-RV7TTTM173",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+    },
+  ],
 };
 
 export default config;
